b24f47d66c752a97569d92f4d9f12e876ae2805b
Théo Vandormael <theo.vandormael@proton.me>
2025-10-13 19:12:50 +0200

    feat(dashboard): prototype the layout

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,17 +1,6 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
-
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
-                </div>
-            </div>
-        </div>
+<x-dashboard-layout>
+    <div class="mb-8">
+        <h1 class="text-3xl font-bold text-slate-900">Dashboard</h1>
+        <p class="mt-2 text-slate-600">Welcome back! Here's an overview of your activity.</p>
     </div>
-</x-app-layout>
+</x-dashboard-layout>
