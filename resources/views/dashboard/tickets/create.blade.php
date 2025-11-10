78b93e407ec57d49215be5fb068128c36683eb56
Théo Vandormael <theo.vandormael@proton.me>
2025-11-10 14:48:35 +0100

    refactor: add type to button for better interactions

--- resources/views/dashboard/tickets/create.blade.php
+++ resources/views/dashboard/tickets/create.blade.php
@@ -2,15 +2,12 @@
     <x-dashboard.page-header title="Ouvrir un nouveau ticket"
         description="Remplissez les informations ci-dessous pour créer un nouveau ticket." />
 
-    <div class="bg-white border-slate-200 shadow-sm border rounded-lg p-12">
+    <div class="p-12 bg-white border rounded-lg shadow-sm border-slate-200">
         <x-form action="{{ route('tickets.store') }}" method="POST">
-
-            @csrf
-
             <x-async-form-field name="customer_id" icon="heroicon-o-user" label="Client concerné"
                 route="{{ route('api.customers.index') }}" placeholder="Rechercher un client..." size="half">
                 <div class="flex items-center space-x-3">
-                    <div class="flex-shrink-0 w-8 h-8 bg-slate-100 rounded-full flex items-center justify-center">
+                    <div class="flex items-center justify-center flex-shrink-0 w-8 h-8 rounded-full bg-slate-100">
                         <x-heroicon-o-user class="w-5 h-5 text-gray-400" />
                     </div>
                     <div>
@@ -36,7 +33,7 @@
 
 
             <x-slot name="actions">
-                <x-button onclick="window.history.back()" color="danger" variant="soft" label="Annuler"
+                <x-button type="button" onclick="window.history.back()" color="danger" variant="soft" label="Annuler"
                     icon="heroicon-o-x-mark" />
                 <x-button type="submit" color="primary" variant="soft" label="Créer le ticket"
                     icon="heroicon-o-check" />
